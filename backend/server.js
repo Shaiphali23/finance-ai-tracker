592319e6291283373e62a61460e6cb95beeadeb2
Shaiphali23 <shaiphalijaiswal978@gmail.com>
2025-09-06 21:18:34 +0530

    UPDATED BACKEND

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,11 @@ connectDB();
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
