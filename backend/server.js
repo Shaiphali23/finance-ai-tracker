b55873f5fee78e22cadb3bf9ecb98d2d6b4df4d4
Shaiphali23 <shaiphalijaiswal978@gmail.com>
2025-09-06 17:22:31 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -16,24 +16,12 @@ const app = express();
 connectDB();
 
 // Middleware
-const allowedOrigins = [
-  "http://localhost:5173",
-  `${process.env.FRONTEND_URL}`
-];
-
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
   })
 );
-
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
